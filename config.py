f8de87ec9b9d82e45ebcf921eec028df55db045a
ankitx1985 <ankitsharma20045@gmail.com>
2025-09-03 14:07:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "26994377"))
-API_HASH = getenv("API_HASH", "9c9eb74a4a0a1ecd4c96abebf3c637ee")
-BOT_TOKEN = getenv("BOT_TOKEN", "7509054778:AAEcs-eHWpie4SWDgxF38yFFL28UVthUeA4")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "8184789731").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://fixmayart834:FMWwXBd4JJYMs2Iv@cluster0.ltpube9.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("")
 LOG_GROUP = getenv("LOG_GROUP", "-1003087587927")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003024428633"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
